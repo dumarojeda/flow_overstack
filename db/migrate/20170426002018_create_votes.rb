269ad6991a5a64c84bb26cbdb9df5b5a8004c1a3
Juan David <rangeljdr@gmail.com>
2017-04-25 20:58:09 -0500

    First seeds

--- db/migrate/20170426002018_create_votes.rb
+++ db/migrate/20170426002018_create_votes.rb
@@ -1,10 +1,14 @@
 class CreateVotes < ActiveRecord::Migration[5.0]
   def change
     create_table :votes do |t|
-      t.integer :count
       t.integer :votable_id
       t.string :votable_type
       t.timestamps
     end
   end
 end
+
+#We don't need :count because every istance is a vote. Count would be used under a different name for weight and direction
+#t.integer :count
+#question.votes
+#question.votes.reduce()
