8d6c702c05bbba1dd6dce357af1c1824adfe0f22
Juan David <rangeljdr@gmail.com>
2017-04-26 19:37:34 -0500

    add association for the User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   #we don't need this association because the user doesn't belong to many models; it HAS many models. Otherwise, we would need to use this polymorphic: belongs_to :userable, :polymorphic => true
+
+  has_many :questions
+  has_many :answers
+  has_many :votes
+  has_many :comments
 
   validates :email, :uniqueness => true, :format => {:with => /\A\b[A-Z0-9\.\_\%\-\+]+@(?:[A-Z0-9\-]+\.)+[A-Z]{2,6}\b\z/i}
 
