51d5cc0665b54a2c113a8a18522f37e0b7cc0917
Dumar Ojeda <dumarojeda@gmail.com>
2017-04-27 15:31:24 -0500

    Fix problem with associations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,8 @@
 class Answer < ApplicationRecord
   has_many :comments, as: :commentable
   has_many :votes, as: :votables
-  belongs_to :question, :user
+  belongs_to :question
+  belongs_to :user
 
   validates :description, presence: true
 
