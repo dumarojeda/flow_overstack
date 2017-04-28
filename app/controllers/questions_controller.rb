2f60eb9339592e2949446d668aff7bddf1674534
Dumar Ojeda <dumarojeda@gmail.com>
2017-04-28 02:58:54 -0500

    Improvements for forms questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-
+  before_action :set_question, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
@@ -7,7 +7,6 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find(params[:id])
   end
 
   def new
@@ -18,15 +17,35 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    question = Question.new(question_params)
-    if question.save
+    @question = Question.new(question_params)
+    if @question.save
       redirect_to root_path
     else
+      @errors = @question.errors.full_messages
       render 'new'
     end
   end
 
+  def destroy
+    @question.destroy
+   
+    redirect_to questions_path
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to questions_path
+    else
+      @errors = @question.errors.full_messages
+      render 'edit'
+    end 
+  end
+
   private
+    def set_question
+      @question = Question.find(params[:id])
+    end
+
     def question_params
       params.require(:question).permit(:title, :description).merge(user_id: current_user.id)
     end
